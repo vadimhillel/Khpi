87caef6eebb36165e00f88f9db0b879ad2985e52
Vadym Pliaka <vadimfootball1304@gmail.com>
2023-11-12 22:22:27 +0100

    📝 control work for KHPIUNIV UPDATED

--- KW/task2.py
+++ KW/task2.py
@@ -1,15 +1,4 @@
-# list1 = [16, 17, 14, 21, 16, 13, 10, 11, 16, 17]
-# list2 = [18, 17, 18, 13, 18, 21, 24, 23, 16, 17]
-# list3 = [0, 0, 2, 0, 0, 0, 0, 0, 2, 0]
 
-# print("Number\t\tlist1\t\tlist2\t\tlist3")
-# print(90*"=")
-
-# for i, element1, element2, element3 in zip(range(9), list1, list2, list3):
-#     print('{:^6}\t\t{:^5}\t\t{:^5}\t\t{:^5}'
-#           .format(i+1, element1, element2, element3))
-
-            
 def function(list1: list, list2: list) -> list:
     print(f"\n1st list is {list1}", 
               f"\n2nd list is {list2}\n")
