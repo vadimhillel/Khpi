62c21c742cd3a55330cef0a914b74b47fe07a8e7
Vadym Pliaka <vadimfootball1304@gmail.com>
2023-11-13 22:25:22 +0100

    📄 lab5 for KHPIUNIV

--- lab5/TasksFor65D/task1.py
+++ lab5/TasksFor65D/task1.py
@@ -1,6 +1,7 @@
 N = ord("V") % 5 + 1
 print(f"\n\t Number of variant is {N}\n")
 
+
 class MyFunctions:
     def __init__(self, list1: list, list2: list) -> None:
         self._list1 = list1
@@ -11,13 +12,13 @@ class MyFunctions:
         for i in self._list2:
             self._list1 = [0 if x==i else x for x in self._list1]
         return self._list1
-    
+
     def func2(self) -> list:
         """for Loop + mapping/lambda, in return list"""
         for i in self._list2:
             self._list1 = list(map(lambda x: x if x != i else 0, self._list1))
         return self._list1
-    
+
     def func3(self) -> list:
         """for Loop + while loop + .index"""
         my_idx = -1
@@ -28,16 +29,20 @@ class MyFunctions:
                     self._list1[my_idx] = 0
                 except ValueError:
                     break
-        
+
         return self._list1
-    
+
     def func4(self) -> list:
         """for Loop + for Loop + List Slicing"""
-        for i in range(len(self._list2)):
+        for i in range(len(self._list1)):
             for j in self._list2:
                 if self._list1[i] == j:
+<<<<<<< HEAD
                     self._list1 = self._list1[:i] + [0] + self._list1[i+1 :]
+=======
+                    self._list1 = self._list1[:i] + [0] + self._list1[i+1:] # flake8: noqa
+>>>>>>> 9ccc87e (📄 lab5 for KHPIUNIV)
         return self._list1
-    
+
     def source_list(self) -> None:
         print("Source list is: ", self._list1)
