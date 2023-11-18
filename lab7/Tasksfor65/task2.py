a10cadebda776f3768af393966c254345ead44a0
Vadym Pliaka <vadimfootball1304@gmail.com>
2023-11-18 12:49:07 +0100

    📄 lab7 for KHPIUNIV

--- lab7/Tasksfor65/task2.py
+++ lab7/Tasksfor65/task2.py
@@ -1,7 +1,7 @@
 from task1 import create_dict
 
-def sum_dict_vals(dict1: dict, str: str) -> dict:
-    return {**dict1, str: sum(dict1.values())}
+def sum_dict_vals(d: dict, s: str) -> dict:
+    return {**d, s: sum(d.values())}
 
 def main():
     print(sum_dict_vals(create_dict(10), 'total'))
