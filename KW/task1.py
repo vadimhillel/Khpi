710ab2d5676235be73cedf9ebf25005f81ac1c82
Vadym Pliaka <vadimfootball1304@gmail.com>
2023-11-26 22:15:52 +0100

    📝 control work for KHPIUNIV UPDATED

--- KW/task1.py
+++ KW/task1.py
@@ -5,17 +5,18 @@ import numpy as np
 N = ord("V") % 5 + 1
 print("\n\tVariant: ", N)
 
+
 def my_func(interval: list, res: list) -> list:
       for x in np.arange(interval[0], interval[1], 0.2):
             if (x > 1):
                   y = np.sqrt(math.tan(x**2 - 1))
-                  res.append(f'x = {x:.2f}'), res.append(f'y = {y:.2f}')
+                  res.append(float(f'{y:.2f}'))
             elif (0<=x<=1):
                   y = -2*x
-                  [res.append(f'x = {x:.2f}'), res.append(f'y = {y:.2f}')]
+                  res.append(float(f'{y:.2f}'))
             elif (x < 0):  
                   y = math.exp(np.cos(x))
-                  [res.append(f'x = {x:.2f}'), res.append(f'y = {y:.2f}')]
+                  res.append(float(f'{y:.2f}'))
             
       return res
 
