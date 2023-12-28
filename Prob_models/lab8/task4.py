612706022c214a2d2939e936d0655dd7bc7e138f
Vadym Pliaka <vadimfootball1304@gmail.com>
2023-12-28 14:20:06 +0100

    📄 lab8 ML

---
+++ Prob_models/lab8/task4.py
@@ -0,0 +1,18 @@
+class Mss:
+    def __init__(self, arrival_rate: int, t: int) -> None:
+        self._arrival_rate = arrival_rate
+        self._t = t
+
+    def service_intensity(self) -> list:
+        return 1 / (1 + (self._arrival_rate / (60 / self._t)))
+    
+    def relative_throughput(self):
+        return self._arrival_rate*(self._t/60)
+        
+def main():
+    m: Mss = Mss(20, 6)
+    print(f"\nThe service intensity is {m.service_intensity():.2f}" +
+          f" and relative throughput is: {m.relative_throughput()}")
+    
+if __name__ == "__main__":
+    main()
