f2e59f478206a339322cecb4fca4e2c9b43b0291
Vadym Pliaka <vadimfootball1304@gmail.com>
2024-03-23 12:02:07 +0100

    Edit

--- lab6/task1.py
+++
@@ -1,5 +0,0 @@
-def vowel_count(string):
-    return f"Amount of vowels: {len([ch for ch in string if ch in 'aeiouAEIOU'])}"
-
-assert(vowel_count("VolodymyrOleksandrovichZelenskyi"))
-print(vowel_count("VolodymyrOleksandrovichZelenskyi"))
--- lab6/task2.py
+++
@@ -1,9 +0,0 @@
-from string import ascii_letters as asl
-
-
-def strfunc(string: str) -> str:
-    return ''.join([asl[(asl.index(i)+1) %len(asl)] if i in asl else i for i in string])
-
-assert(strfunc("VolodymyrOleksandrovichZelenskyi"))
-print(strfunc("VolodymyrOleksandrovichZelenskyi"))
-             
